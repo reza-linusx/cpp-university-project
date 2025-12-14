4baa87e66e1289a8840407c1122bd1d1ba6e17df
Reza <reza.linusx@gmail.com>
2025-12-14 21:35:25 +0330

    Display app intro

--- app.cpp
+++ app.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world!";
+	cout << "=== Physics & Math Helper ===\n";
+	cout << "Choose an option:\n";
+	cout << "1. Quadratic Equation\n";
+	cout << "2. Kinematics\n";
+	cout << "3. Vectors\n";
+	cout << "Enter your choice (1-3): ";
+
 	return 0;
 }
